d06b6414ddb64a846519ac329669408516b32375
Anish Bar <baranish2003@gmail.com>
2026-05-13 19:27:14 +0530

    fix: update heading color to blue for better visibility

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="bg-gray-100 min-h-screen h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-center">Project AI</h1>
+      <h1 className="text-3xl text-blue-500 font-bold text-center">Project AI</h1>
     </div>
   );
 };
